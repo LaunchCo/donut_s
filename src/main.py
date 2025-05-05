fc249ddf970a399b76b6f9d81951f673ddd89ed4
jackson338 <jacksondoaks@gmail.com>
2025-05-05 17:27:16 -0600

    Changed port

--- src/main.py
+++ src/main.py
@@ -102,4 +102,4 @@ generate_kwargs = gen_config.to_dict()
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=9000)
